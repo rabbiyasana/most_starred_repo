ffd4da001248e88f9df17f9278b6f808955c6ca9
rabbiyasana <rabbiasana55@gmail.com>
2025-05-20 22:20:57 +0500

    dynamic api url structure

--- most_starred_repo/src/utilities/api.jsx
+++ most_starred_repo/src/utilities/api.jsx
@@ -1,2 +1,11 @@
 import axios from 'axios';
 import { GITHUB_API_URL } from '../constants/constants.jsx';
+
+export const fetchRepos = async (page = 1) => {
+  const date = new Date();
+  date.setDate(date.getDate() - 10);
+  const formattedDate = date.toISOString().split('T')[0];
+
+  const response = await axios.get(`${GITHUB_API_URL}?q=created:>${formattedDate}&sort=stars&order=desc&page=${page}`);
+  return response.data.items;
+};
